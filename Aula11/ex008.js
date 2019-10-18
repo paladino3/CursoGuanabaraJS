a371f5989042cf720f77147b82cedd2be026c86f
Wesley rolim simoes <rolim.r@icloud.com>
2019-10-18 03:31:06 -0300

    18/10 3:

---
+++ Aula11/ex008.js
@@ -0,0 +1,15 @@
+
+
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60)
+{
+
+console.log(`Voce ultrapassou a velocidade praticada, Multado!`)
+
+}  
+  
+console.log(`Dirija sempre usando o cinto de seguranca!`)
+
---
+++ Aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'França'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais != 'brasil'){
+    console.log('vocé estrangeiro!')
+}else{
+    console.log('voce é brasileiro!')
+}
